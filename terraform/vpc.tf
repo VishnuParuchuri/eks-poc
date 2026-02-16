2fbd209a74f0763187efa85b24ac7b67440e055f
Vishnu Vardhan <pvsvv1994@outlook.com>
2026-02-16 19:12:31 +0530

    Fixed terraform formatting after security updates

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,9 +15,9 @@ resource "aws_vpc" "main" {
 # Public Subnets (Multi AZ)
 # -------------------------
 resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-1a"
   # tfsec:ignore:aws-ec2-no-public-ip-subnet
   map_public_ip_on_launch = true
 
@@ -29,9 +29,9 @@ resource "aws_subnet" "public_1" {
 }
 
 resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-1b"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-1b"
   # tfsec:ignore:aws-ec2-no-public-ip-subnet
   map_public_ip_on_launch = true
 
