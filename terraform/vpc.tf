dd2e1efc959f8a71a17f47226ec76430a4b8ba73
Vishnu Vardhan <pvsvv1994@outlook.com>
2026-02-17 13:36:27 +0530

    Fixed AZ issue

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
   # tfsec:ignore:aws-ec2-no-public-ip-subnet
   map_public_ip_on_launch = true
 
@@ -61,7 +61,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name                                        = "private-subnet-2"
